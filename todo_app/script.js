ca7da2244e7986ae130cee33b108667a3223b912
Ghafoor Nazari <ghafoornazari@proton.me>
2025-09-10 00:25:01 +0200

    Updated the script file

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,9 +1,39 @@
-function getValue() {
-  let input = document.getElementById("newHabit");
-  let value = input.value;
+let items = ["hello1", "hello2", "hello3"];
 
-  //alert("New habit: " + value);
-  input.value = "";
-	
-	document.querySelector("#habit").innerHTML = value
+const itemsDiv = document.getElementById("items");
+
+function renderItems() {
+  itemsDiv.innerHTML = null;
+
+  for (const [index, item] of Object.entries(items)) {
+    const container = document.createElement("div");
+
+    const task = document.createElement("p");
+    let count = parseInt(index) + 1;
+    task.textContent = count + ". " + item;
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.onclick = () => removeItem(index);
+
+    container.appendChild(task);
+    container.appendChild(button);
+
+    itemsDiv.appendChild(container);
+  }
 }
+
+function removeItem(index) {
+  items.splice(index, 1);
+  renderItems();
+}
+
+function addItem() {
+  let newItem = document.getElementById("input");
+  let newTask = newItem.value;
+  console.log(newTask);
+  items.push(newTask);
+  newItem.value = "";
+  renderItems();
+}
+renderItems();
